7f094cebb11a4a0e3158fd0e373a5207bc0c0bef
Ian Holmes <ihh@berkeley.edu>
2020-03-15 17:13:55 -0700

    example

--- src/render.js
+++ src/render.js
@@ -1,4 +1,13 @@
 const { render } = (() => {
+
+  const colorScheme = {
+    clustal: { G: "orange", P: "orange", S: "orange", T: "orange", H: "red", K: "red", R: "red", F: "blue", W: "blue", Y: "blue", I: "green", L: "green", M: "green", V: "green" },
+lesk: { G: "orange", A: "orange", S: "orange", T: "orange", C: "green", V: "green", I: "green", L: "green", P: "green", F: "green", Y: "green", M: "green", W: "green", N: "magenta", Q: "magenta", H: "magenta", D: "red", E: "red", K: "blue", R: "blue" },
+maeditor: { A: "lightgreen", G: "lightgreen", C: "green", D: "darkgreen", E: "darkgreen", N: "darkgreen", Q: "darkgreen", I: "blue", L: "blue", M: "blue", V: "blue", F: "lilac", W: "lilac", Y: "lilac", H: "darkblue", K: "orange", R: "orange", P: "pink", S: "red", T: "red" },
+    cinema: { H: "blue", K: "blue", R: "blue", D: "red", E: "red", S: "green", T: "green", N: "green", Q: "green", A: "white", V: "white", L: "white", I: "white", M: "white", F: "magenta", W: "magenta", Y: "magenta", P: "brown", G: "brown", C: "yellow", B: "gray", Z: "gray", X: "gray", "-": "gray" }
+  }
+
+  const defaultColorScheme = "maeditor"
   
   const render = (opts) => {
     // opts.branches is a list of [parent,child,length]
@@ -17,6 +26,8 @@ const { render } = (() => {
     const collapsedNodeHandleFillStyle = opts.collapsedNodeHandleFillStyle || 'black'
     const rowConnectorDash = opts.rowConnectorDash || [2,2]
     const handler = opts.handler || {}
+    const color = opts.color || colorScheme[opts.colorScheme || defaultColorScheme]
+
     const lineWidth = 1
     const availableTreeWidth = treeWidth - nodeHandleRadius - 2*lineWidth
     const scrollbarHeight = 20  // hack
@@ -35,7 +46,7 @@ const { render } = (() => {
       if (!node)
         throw new Error ("All nodes must be named")
       if (nodeRank[node])
-        throw new Error ("All node names must be unique")
+        throw new Error ("All node names must be unique (duplicate '" + node + "')")
       nodeRank[node] = nodes.length
       nodes.push (node)
     }
@@ -98,7 +109,10 @@ const { render } = (() => {
         if (rowHeight[node])
           nameDiv.innerText = node
         if (rowData[node])
-          rowDiv.innerText = rowData[node]
+          rowData[node].split('').forEach ((c) => {
+            let span = create ('span', rowDiv, { color: color[c.toUpperCase()] || color['default'] || 'black' })
+            span.innerText = c
+          })
         if (!children[node].length) {
           ctx.setLineDash ([])
           ctx.beginPath()
